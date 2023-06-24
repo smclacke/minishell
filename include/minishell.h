926777b53c98efc2dafabf7a66b6a53ab8bcec55
Sarah <115113929+smclacke@users.noreply.github.com>
2023-06-24 19:29:06 +0200

    Add files via upload

---
+++ include/minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   minishell.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/24 19:20:16 by smclacke      #+#    #+#                 */
+/*   Updated: 2023/06/24 19:23:07 by smclacke      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "libft/src/libft.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#endif
