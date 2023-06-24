747b664151c6f446a908e8b076fbcdb3a6c98f5c
Sarah Mclacken <smclacke@f0r4s1.codam.nl>
2023-06-24 19:33:15 +0200

    just making some nice files :)

--- include/minishell.h
+++ include/minishell.h
@@ -6,14 +6,14 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/24 19:20:16 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/06/24 19:23:07 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/06/24 19:32:34 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include "libft/src/libft.h"
+#include "libft_sarah/src/libft.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
