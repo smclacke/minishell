ca7a796ea66c685b9c28deb7553810291354ff29
Sarah Mclacken <smclacke@f0r3s11.codam.nl>
2023-06-25 20:17:19 +0200

    comments

--- src_saar/main_saar.c
+++ src_saar/main_saar.c
@@ -6,15 +6,20 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/24 19:23:45 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/06/25 16:38:33 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/06/25 20:17:09 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// use struct as global?
+
+// only use ./djoyke and ./sarah for the start, get used to branches, 
+// then only focus on ./minishell, use these spaces for comments and tests
+
+// write test script (!)
+
 int	main()
 {
 	printf("I'm Sarah's dummy main\n");
 }
-
-
