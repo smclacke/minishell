3c6ab9867518052e80abbde885e4f2406ed2cf55
DjoykeAbyah <crudendjoyke@gmail.com>
2023-06-27 19:48:30 +0200

    pseudocode start

--- src_djoy/main_djoy.c
+++ src_djoy/main_djoy.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/24 19:24:05 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/06/26 13:29:07 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/06/27 19:35:21 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,36 @@
 
 // hey djoyke, here's a main just for you :*
 
-int	main()
+int	main(void)
 {
 	printf("I'm Djoyke's dummy main\n");
 	printf("There are some new things, let's see how this goes\n");
 }
+
+	/* parent process
+
+	will get the structs and based on how many I need to fork.
+	needs to inherit all the basics.
+	open all pipes and fd's in here
+
+	Start by creating a new process for each command in the pipeline 
+	and making the parent wait for the last command. 
+	This will allow running simple commands such as “ls ­al”.
+
+	{
+		taking whatever from a struct();
+		{
+			error check;
+		}
+		function that scales the pipes based on the amount of commands()
+		{
+			{
+				//make macro's for the read and write end in header file;
+				opening the pipes();
+			}
+		error check
+		function that forks x commands();
+		waitstatus();
+	}
+	
+	*/
