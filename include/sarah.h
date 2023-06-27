f7296b3ea4aebfd97e3fba135ac64966a9eb1e02
Sarah Mclacken <smclacke@f1r2s9.codam.nl>
2023-06-27 13:47:12 +0200

    beautiful prompt haw haw

--- include/sarah.h
+++ include/sarah.h
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/26 14:10:39 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/06/27 11:52:37 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/06/27 13:46:11 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,17 @@
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 
-# define PROMPT "hey Djoyke $"
+# define PROMPT YELLOW".~❃~."PURPLE".~⚘~."CYAN".~✿~."RESET
 
+// Colours
+# define RESET "\033[0m"
+# define RED "\033[1;91m"
+# define GREEN "\033[1;92m"
+# define YELLOW "\033[1;93m"
+# define BLUE "\033[1;94m"
+# define PURPLE "\033[1;95m"
+# define CYAN "\033[1;96m"
+# define WHITE "\033[1;97m"
+# define BLACK "\033[1;90m"
 
 #endif
