023c512bd5d00fda44a7dc8e55b88adc66066362
DjoykeAbyah <crudendjoyke@gmail.com>
2023-06-28 12:26:33 +0200

    headerfile changes

--- include/minishell.h
+++ include/minishell.h
@@ -6,23 +6,26 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/24 19:20:16 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/06/27 12:53:23 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/06/28 12:25:10 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include "libft/src/libft.h"
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
+# include "libft/src/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <stdbool.h>
 
 # define PROMPT "hey Djoyke $"
+# define READ 0
+# define WRITE 1
 
 #endif
