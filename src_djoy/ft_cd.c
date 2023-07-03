2f048f26886bbc9f091fbcc0785d5c2e8317c2c0
DjoykeAbyah <crudendjoyke@gmail.com>
2023-07-03 12:45:07 +0200

    yes

--- src_djoy/ft_cd.c
+++ src_djoy/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/03 10:12:26 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/07/03 12:09:55 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/07/03 12:44:49 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
