4e91af6d653f6d13221872e9eeb2c4762ba831f9
DjoykeAbyah <crudendjoyke@gmail.com>
2023-07-03 12:55:18 +0200

    notes added

--- src_djoy/ft_cd.c
+++ src_djoy/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/03 10:12:26 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/07/03 12:44:49 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/07/03 12:54:33 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,36 @@ void	ft_cd(char **argv)
 {
 	
 }
+
+//		{
+//     char cwd[256];  // Buffer to hold the current working directory
+
+//     if (getcwd(cwd, sizeof(cwd)) != NULL) {
+//         printf("Current working directory: %s\\n", cwd);
+//     } else {
+//         perror("getcwd() error");
+//         return 1;
+//     }
+
+//     // Rest of the program...
+
+//     return 0;
+// }
+
+// {
+//     const char *path = "/home/user/documents";  
+		// Replace with the desired directory path
+
+//     // Change the current working directory to the specified path
+//     int result = chdir(path);
+
+//     if (result == 0) {
+//         printf("Directory changed successfully.\\n");
+//     } else {
+//         perror("Directory change failed");
+//     }
+
+//     // Rest of the program...
+
+//     return 0;
+// }
