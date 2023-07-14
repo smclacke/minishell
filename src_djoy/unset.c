25f410b4eeba11049fd37f5306d1d6d5ba1f1472
DjoykeAbyah <crudendjoyke@gmail.com>
2023-07-14 17:37:56 +0200

    tired

--- src_djoy/unset.c
+++ src_djoy/unset.c
@@ -6,9 +6,19 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/13 15:27:08 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/07/13 15:27:21 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/07/14 17:34:57 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/djoyke.h"
 
+//unset values and attributes of variables and functions
+/**
+ * @param argv terminal given arguments (for now)
+ * @param env pointer to environment
+ * @brief unset values and attributes of variables and functions
+*/
+void	ft_unset(char *argv, t_env *env)
+{
+	
+}
--- src_djoy/utils.c
+++ src_djoy/utils.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/03 10:13:16 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/07/13 18:09:13 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/07/14 17:18:50 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,8 @@ void	check_for_builtin(char **argv, t_env *env)
 		ft_pwd(NULL);
 	if (ft_strcmp(argv[1], "export") == 0)
 		ft_export(argv, env);
+	if (ft_strcmp(argv[1], "unset") == 0)
+		ft_unset(argv[1], env);
 	else
 		exit(EXIT_FAILURE);
 }
