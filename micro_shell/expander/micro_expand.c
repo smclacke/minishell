390a44e2a7a9caa9fbe834fb67de62c691cafffc
DjoykeAbyah <crudendjoyke@gmail.com>
2023-08-03 17:29:44 +0200

    cleaned my crap hehehehhehe

--- micro_shell/expander/micro_expand.c
+++ micro_shell/expander/micro_expand.c
@@ -6,40 +6,11 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/31 19:20:06 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/08/03 14:13:06 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/08/03 17:28:50 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-// /**
-//  * @param node linked list
-//  * @param env string or char to compare with
-//  * @brief checks arguments to find meta_chars: 
-//  * $, >>, <<, >, <, |
-//  * @todo 
-//  * 1) needs to be passed to actual process,
-//  * 2) MAYBE MAKE IT A BOOL?
-// */
-// bool	micro_check_for_meta(t_parser *node)
-// {
-// 	if (node->sign == NULL)
-// 		return (false);
-// 	if (micro_strcmp(node->sign, "$") == 0)
-// 		return (true);
-// 	else if (micro_strcmp(node->sign, ">>") == 0)
-// 		return (true);
-// 	else if (micro_strcmp(node->sign, "<<") == 0)
-// 		return (true);
-// 	else if (micro_strcmp(node->sign, ">") == 0)
-// 		return (true);
-// 	else if (micro_strcmp(node->sign, "<") == 0)
-// 		return (true);
-// 	else if (micro_strcmp(node->sign, "|") == 0)
-// 		return (true);
-// 	else
-// 		return (false);
-// }
 
 /**
  * @param node linked list
@@ -68,81 +39,3 @@ bool	shelly_check_for_builtin(t_parser *lst)
 	else
 		return (false);
 }
-
-// /**
-//  * @param lst linked list to loop through
-//  * @brief loops to list to go to last position
-// */
-// t_expand	*shelly_expand_lstlast(t_expand *lst)
-// {
-// 	if (!lst)
-// 		return (NULL);
-// 	while (lst->next)
-// 		lst = lst->next;
-// 	return (lst);
-// }
-
-// /**
-//  * @param lst linked list
-//  * @param new new node to be added to linked list 
-//  * @brief loops through list to add the new node to the back
-//  * @todo adding previous in case of doubly linked list
-// */
-// void	shelly_expand_lstadd_back(t_expand **lst, t_expand *new)
-// {
-// 	t_expand	*last;
-
-// 	if (*lst)
-// 	{
-// 		last = shelly_expand_lstlast(*lst);
-// 		last->next = new;
-// 	}
-// 	else
-// 		*lst = new;
-// }
-
-// t_expand	*init_expand_list(t_parser *lst)
-// {
-// 	t_expand	*new;
-
-// 	new = (t_expand *)malloc(sizeof(*new));
-// 	if (!new)
-// 		micro_error("malloc", errno);
-// 	if (lst->sign != NULL)
-// 		new->sign = lst->sign;
-// 	// else
-// 	// 	new->sign = NULL;
-// 	new->str = lst->str;
-// 	if (shelly_check_for_builtin(lst))
-// 		new->builtin = lst->cmd;
-// 	else
-// 		new->builtin = NULL;
-// 	new->cmd = NULL;
-// 	new->next = NULL;
-// 	printf("new->str = [%s]\n", new->str);
-// 	printf("new->sign = [%s]\n", new->sign);
-// 	printf("new->builtin = [%s]\n", new->builtin);
-// 	return (new);
-// }
-
-// /**
-//  * @param envp environment pointer
-//  * @param lst list with parsed tokens
-//  * @brief makes expand list by checking the nodes parsed
-//  * @todo 
-//  * 1) do I need to do this in a loop? everything is now in one node
-//  * 2) expand(mini->tokens) // tokens from s_parser struct, 
-//  * 3) check built-in, check meta char, check quotes.
-//  * 4) see if i need to fork, just execute or even redirect input output
-// */
-// t_expand	*micro_expand(t_parser *lst)
-// {
-// 	t_expand	*expand_node;
-// 	t_expand	*expand_head;
-
-// 	expand_node = NULL;
-// 	expand_head = NULL;
-// 	expand_node = init_expand_list(lst);
-// 	shelly_expand_lstadd_back(&expand_head, expand_node);
-// 	return (expand_head);
-// }
--- micro_shell/main.c
+++ micro_shell/main.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/25 12:11:57 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/08/03 14:36:24 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/08/03 17:29:30 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,11 @@
 
 // INPUT: e'ch'o abc$abc 
 // OUTPUT: abc
-
-// Djoyke, when checking vadility of the rest of the input, after first peice of input, i can send
-// to you what is what and then you expand it, if its not good in expanding process, then error thrown
-// sarah: try explain this so it makes sense....
-
 int	main(int argc, char **argv, char **envp)
 {
 	char		*input;
 	t_lexer		*tokens;
 	t_parser	*parser_struct;
-	// t_expand	*expand_list;
 
 	(void) argc;
 	(void) argv;
@@ -35,30 +29,16 @@ int	main(int argc, char **argv, char **envp)
 	{
 		input = readline(PROMPT);
 		add_history(input);
-
-		//-- Sarah --//
 		tokens = lexer(input);
 		if (!tokens)
 			continue ;
 		print_lexer(tokens);
-		
-
 		parser_struct = parser(tokens);
 		if (!parser_struct)
 			continue ;
-		
-
-
-
-
-		//-- Djoyke --//
-		// expand(mini->tokens) // tokens from s_parser struct, 
-		//	check built-in, check meta char, check quotes.
-		if (shelly_check_for_builtin(parser_struct))//will be a expand funct
+		if (shelly_check_for_builtin(parser_struct))
 			printf("there's a builtin whoop\n");
-		// execution // make env_list, create child processes, execve
 		micro_execute(envp, parser_struct);
-		// free input (readline needs to be fred at end)	
 	}
 	return (0);
 }
