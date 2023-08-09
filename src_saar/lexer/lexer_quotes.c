4858e4483e8799bc9e42a2329499c5deaca7b116
Sarah Mclacken <smclacke@f1r4s5.codam.nl>
2023-08-09 17:09:44 +0200

    still fucked

--- src_saar/lexer/lexer_quotes.c
+++ src_saar/lexer/lexer_quotes.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/12 17:07:01 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/08/09 16:43:44 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/08/09 17:08:40 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,8 @@ static char	*make_quotes(char *input)
 	int		len;
 	char	*quotes;
 
-	len = quote_length(input);
+	// len = quote_length(input);
+	len = ft_strlen(input);
 	quotes = (char *)malloc(sizeof(char) * (len + 1));
 	while (i < len)
 	{
@@ -51,29 +52,57 @@ static char	*make_quotes(char *input)
  * @param	input from readline to tokenise
  * @return	array of split up words with quotes handled separately including spaces within quotes
 */
+// char	**ft_split_shelly(char *input)
+// {
+// 	char	**array;
+// 	int		i = 0;
+
+// 	array = (char **)malloc(sizeof(char *) * (lq_count_words(input) + 1));
+// 	if (!array)
+// 		return (0);
+// 	while (*input)
+// 	{
+// 		while (*input && lq_what_to_split(*input))
+// 			input++;
+// 		if (*input && !ft_isquote(*input))
+// 		{
+// 			array[i] = make_words(input);
+// 			i++;
+// 		}
+// 		if (ft_isquote(*input))
+// 		{
+// 			array[i] = make_quotes(input);
+// 			i++;
+// 		}
+// 		while (*input && !lq_what_to_split(*input) && !ft_isquote(*input))
+// 			input++;
+// 	}
+// 	array[i] = 0;
+// 	return (array);
+// }
+
 char	**ft_split_shelly(char *input)
 {
 	char	**array;
 	int		i = 0;
+	int		j = 0;
 
 	array = (char **)malloc(sizeof(char *) * (lq_count_words(input) + 1));
 	if (!array)
 		return (0);
 	while (*input)
 	{
-		while (*input && lq_what_to_split(*input))
+		while (*input && lq_what_to_split(*input) && !ft_isquote(*input))
+		{
 			input++;
+			j++;
+		}
 		if (*input && !ft_isquote(*input))
 		{
 			array[i] = make_words(input);
 			i++;
 		}
-		if (ft_isquote(*input))
-		{
-			array[i] = make_quotes(input);
-			i++;
-		}
-		while (*input && !lq_what_to_split(*input) && !ft_isquote(*input))
+		while (*input && !lq_what_to_split(*input))
 			input++;
 	}
 	array[i] = 0;
