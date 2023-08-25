34fc40e8cfe74e857a679f20a0b7072913e37134
DjoykeAbyah <crudendjoyke@gmail.com>
2023-08-25 15:46:45 +0200

    cd probably done

--- src_djoy/builtin/ft_unset.c
+++ src_djoy/builtin/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/14 17:53:37 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/08/25 14:40:29 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/08/25 15:36:18 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,3 +73,31 @@ void	mini_remove_env(char *str, t_env **env)
 		}
 	}
 }
+
+// !! if same keyname overwrite that with new else add
+
+// bash: export: `9h=haha': not a valid identifier
+// bash-3.2$ unset 999
+// bash-3.2$ export djoyke =gek
+// bash: export: `=gek': not a valid identifier
+// bash-3.2$ export djoyke=gek gggg (working)
+// bash-3.2$ env (working)
+// djoyke=gek (working)
+// bash-3.2$ export djoyke="gek gggg" (two words if it's in quotations)
+// bash-3.2$ env
+// env part:
+// djoyke=gek gggg
+// bash-3.2$
+// bash-3.2$ export djoyke= gek (working)
+// bash-3.2$ env (working)
+// djoyke= (working)
+
+/* make function for unset and export checking if 
+key (word) has:
+	 capital (H), lowercase (h), underscore (_) as first letter 
+else give error if given 99= as key
+	 bash: export: `99=': not a valid identifier
+	 rest of word can only have numbers, letters and underscore.
+else give error export d@@=haha
+	bash: export: `d@@=haha': not a valid identifier
+same for unset */
