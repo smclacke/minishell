4c9705d3c47d7153ccc397a7f388ff7a2bd74ae5
Sarah Mclacken <smclacke@f0r2s9.codam.nl>
2023-09-13 22:01:59 +0200

    really productive few hours of writing bullshit well done

--- src_saar/lexer/splitting.c
+++ src_saar/lexer/splitting.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/12 17:39:28 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/09/13 20:40:27 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/09/13 21:58:59 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,6 @@
 		 // WE MUST SEPARATE METAS
 	// split on spaces, split on metas but add metas to token array
 	// if quotes, split those apart keep them totally intact
-
-static int	is_token(char *input)
-{
-	if (*input == '|')
-		return (1);
-	if (*input == '<')
-	{
-		if (!ft_strncmp(input, "<<", 2))
-			return (1);
-		return (2);
-	}
-	else if (*input == '>')
-	{
-		if (!ft_strncmp(input, ">>", 2))
-			return (1);
-		return (2);
-	}
-	return (0);
-}
 
 static int	len_token(char *input)
 {
@@ -47,12 +28,14 @@ static int	len_token(char *input)
 	{
 		if (ft_isquote(input[i]))
 		{
+			i++;
 			i += next_quote(&input[i], input[i]);
+			printf("LT | index = %i\n", i);
 			return (i);
 		}
+		printf("this is fucked\n");
 		i++;
 	}
-	printf("index = %i\n", i);
 	return (i);
 }
 
@@ -62,10 +45,11 @@ static char	*give_tokens(char *input)
 	int		token_len;
 
 	token_len = len_token(input);
+	printf("GT | token_len = %i\n", token_len);
 	token = ft_substr(input, 0, token_len);
 	if (!token)
 		return (NULL);
-	printf("give_token: %s\n", token);
+	printf("GT | give_token: %s\n", token);
 	input += token_len;
 	return (token);
 }
@@ -75,13 +59,27 @@ static int	amount_tokens(char *input)
 	int	i = 0;
 	int	count = 0;
 
+	printf("AT | INPUT = %s\n", input);
 	while (input[i])
 	{
-		count += len_token(&input[i]);
+		i += len_token(&input[i]);
+		count++;
+		printf("AT | count = %i\n", count);
+	}
+	return (count);
+}
+
+static bool	annoying_split(char *input)
+{
+	int	i = 0;
+	
+	while (input[i])
+	{
+		if (ft_isquote(input[i]) || is_meta(&input[i]))
+			return (true);
 		i++;
 	}
-	printf("count = %i\n", count);
-	return (count);
+	return (false);
 }
 
 /**
@@ -95,30 +93,24 @@ static int	amount_tokens(char *input)
 char	**parse_input(char *input)
 {
 	char	**array = NULL;
+	int		no_tokens = 0;
 	int		i = 0;
 
-	while (input[i])
+	if (annoying_split(input))
 	{
-		if (ft_isquote(input[i]) || is_meta(&input[i]))
+		no_tokens = amount_tokens(input);
+		array = (char **)malloc(sizeof(char *) * no_tokens + 1);
+		while (i < no_tokens)
 		{
-			i = 0;
-			array = (char **)malloc(sizeof(char *) * amount_tokens(input));
-			while (input)
-			{
-				array[i] = give_tokens(input);
-				printf("parse_input_array: %s\n", array[i]);
-				i++;
-			}
-			if (!array)
-				return (NULL);
-			return (array);
+			printf("i = %i | no_tokens = %i\n", i, no_tokens);
+			array[i] = give_tokens(input);
+			printf("PI | array =  %s\n", array[i]);
+			i++;
 		}
-		i++;
 	}
-	array = ft_split(input, ' ');
+	else
+		array = ft_split(input, ' ');;
 	if (!array)
 		return (NULL);
 	return (array);
 }
-
-	// printf("whats up?\n");
