278d796907f1e4526818b1ae12cde4a9dfdc6797
smclacke <sarahmclacken@gmail.com>
2023-10-17 16:36:03 +0200

    restored djoyke's header and no longer using it, expander is in saar_src now, executor is VERY different, makefilefor djoy and saar also very different now

--- include/djoyke.h
+++ include/djoyke.h
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/28 14:04:53 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/10/17 16:26:28 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/10/17 16:29:50 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,20 @@
 # include "libft/include/libft.h"
 # include "prompt.h"
 # include "colour.h"
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <sys/wait.h>
-#include <stdbool.h>
-#include <errno.h>
+// # include "sarah.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <errno.h>
+# include <stdbool.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 # define READ 0
 # define WRITE 1
@@ -37,14 +39,6 @@
 
 # define TRUE 1
 # define FALSE 0
-
-# define PIPE "|"
-# define MORE ">"
-# define MOREMORE ">>"
-# define LESS "<"
-# define LESSLESS "<<"
-# define DOUBLE_Q "\""
-# define SINGLE_Q "\'"
 
 /**
  * @brief	specifies the different variable types of tokens from the
@@ -61,58 +55,47 @@ typedef struct s_parser
 	char				*cmd;
 	char				*meta;
 	char				*file;
-	char				*str;;
+	char				*str;
 	struct s_parser		*next;
 }				t_parser;
 
-// --------------------------------------------//
-// -------------------UTILS--------------------//
-void				free_tokens(t_parser *tokens);
-// --------------------------------------------//
+// utils
+void			free_tokens(t_parser *tokens);
 
-
-// -------------------LEXER--------------------//
-//-------------------- lexer -----------------//
-t_parser			*lexer(char *input);
+// lexer
+//---------- lexer ----------//
+t_parser		*lexer(char *input);
 
 //-------- lexer_utils --------//
-t_parser			*lexer_listlast(t_parser *list);
-void				lexer_listadd_back(t_parser **list, t_parser *new);
-t_parser			*lexer_listnew(void *input);
-t_parser			*shelly_print_list(t_parser *token);
+t_parser		*lexer_listlast(t_parser *list);
+void			lexer_listadd_back(t_parser **list, t_parser *new);
+t_parser		*lexer_listnew(void *input);
+t_parser		*shelly_print_list(t_parser *token);
 
 //---------- token ----------//
-char				**parse_input(char *input);
+char			**parse_input(char *input);
 
 //-------- token_size --------//
-int					start_token(char *input, int old_start);
-int					len_token(char *input, int len);
+int				start_token(char *input, int old_start);
+int				len_token(char *input, int len);
 
 //-------- token_utils --------//
-int					is_meta(char *input);
-int					space_or_meta(int c);
-int					is_same_quote(int c, char *quote_type);
-char				*which_quote(char *input);
-int					next_quote(char *input, char c);
-// -------------------LEXER--------------------//
-// --------------------------------------------//
+int				is_meta(char *input);
+int				space_or_meta(int c);
+char			*which_quote(char *input);
+int				next_quote(char *input, char c);
 
-
-// -------------------PARSER--------------------//
+// parser
 //-------- parser --------//
-t_parser			*parser(t_parser *tokens);
+t_parser		*parser(t_parser *tokens);
 
 //-------- parser_utils --------//
-t_parser			*handle_pipe(t_parser *data, int *flag);
-int					is_pipe(void *input);
-char				*is_redirect(void *input);
-t_parser			*shelly_parser_print(t_parser *tokens);
-// -------------------PARSER--------------------//
-// --------------------------------------------//
+t_parser		*handle_pipe(t_parser *data, int *flag);
+int				is_pipe(void *input);
+char			*is_redirect(void *input);
+t_parser		*shelly_parser_print(t_parser *tokens);
 
 
-// --------------------------------------------//
-// --------------------------------------------//
 //---- Executor ----//
 typedef struct s_env
 {
@@ -120,7 +103,6 @@ typedef struct s_env
 	char				*value;
 	char				*full;
 	struct s_env		*next;
-	struct s_env		*previous;
 	int					has_value;
 }							t_env;
 
@@ -128,84 +110,42 @@ typedef struct s_env
 typedef struct s_expand
 {
 	char				*before_dollar;
-	char				*var;
 	char				*env_value;
 	char				*comp_str;
 	int					i;
 	int					j;
 }							t_expand;
-// --------------------------------------------//
-// --------------------------------------------//
 
-// -------------------EXPAND--------------------//
-//---------- quotes ----------//
-char				*remove_quotes(char *str);
-void				expand_quotes(t_parser *tokens);
-
-//-------- quote_utils --------//
-void				increment(int *len, int *i);
-int					check_quotes(char *str);
-int					check_space(char *str);
-int					quote_type(int str);
-int					len_quotes(char *str);
-
-typedef struct s_exp_dol
+//----Execution----//
+typedef struct s_execute
 {
-	char	*unassed;
-	char	*expand;
-	char	*dont_expand;
-	char	*str_before_dol;
-	char	*str_after_dol; // str in quotes or dollar in quotes with str after
-}		t_exp_dol;
+	int				fd_in;
+	int				fd_out;
+	int				fork_pid;
+	int				pipe_fd[2];
+	char			**path;
+	char			**env_array;
+	int				in;
+	int				out;
+}						t_execute;
 
-//------------------ expand_dollar ------------------//
+void			free_remain_struct(t_expand *data);
+void			get_compare_str(char *str, t_expand *exp, int i, int j);
+void			reassing_before_dollar(t_expand *exp);
 char			*dollar(char *str, t_env **env, t_expand *exp, int len);
-
-//-------------- expand_dollar_quotes --------------//
-int				check_at_len(char *str, t_expand *exp, int i, int len);
-void			get_before_dollar(char *str, t_expand *exp, int i);
-void			get_compare_str(char *str, t_expand *exp, int i, int j);
-char			*check_if_expand(char *str);
-
-
-//------------------ dollar_utils ------------------//
-void			reassing_before_dollar(t_expand *exp);
-void			reassing_before_dollar_with_var(t_expand *exp);
-int				get_check_value(t_expand *exp, t_env **env);
 char			*return_exp(char *str, t_expand *exp);
-void			save_expanded(t_expand *exp);
-
-
-
-//------------------ ft_expand -------------------//
-void			ft_expand(t_parser *lst, t_env **env);
-
-//------------------ utils ------------------//
-void			free_remain_struct(t_expand *data);
-bool			check_for_builtin(t_parser *lst);
-bool			check_for_meta(t_parser *lst);
-// -------------------EXPAND--------------------//
-// --------------------------------------------//
-
-
-
-
-// -----------------------------------------------//
-// -----------------------------------------------//
-// -----------------------------------------------//
-void			init_expand_struct(t_expand *data);
-void			free_remain_struct(t_expand *data);
-void			get_compare_str(char *str, t_expand *exp, int i, int j);
-void			reassing_before_dollar(t_expand *exp);
-// void			dollar(char *str, t_env **env, t_expand *exp, int len);
-// void			return_exp(char *str, t_expand *exp);
 void			get_before_dollar(char *str, t_expand *exp, int i);
 int				get_check_value(t_expand *exp, t_env **env);
 bool			check_for_meta(t_parser *lst);
 void			ft_expand(t_parser *lst, t_env **env);
-bool			check_for_builtin(t_parser *lst);
+bool			check_for_builtin(t_parser *node);
+bool			check_for_env_builtin(t_parser *node);
+// bool			check_for_child_builtin(t_parser *node);
 void			save_expanded(t_expand *exp);
-void			exp_dollar(char *str, t_env **env, t_expand *exp, int len);
+char			*exp_dollar(char *str, t_env **env, t_expand *exp, int len);
+void			redirect(t_parser *head, t_env **env, t_execute *data);
+void			redirect_outfile(t_parser *head, t_execute *data);
+void			redirect_infile(t_parser *head, t_execute *data);
 
 //----Environment----//
 t_env			*env_list(char **envp, t_env *env);
@@ -219,6 +159,7 @@ void			print_list_value(t_env *env);
 char			**list_to_string(t_env *env);
 void			print_env_list(t_env *lst);
 void			print_list_full(t_env *env);
+void			free_env(t_env **lst);
 
 //---- Built-in ----//
 void			free_all(t_env *env);
@@ -236,23 +177,18 @@ void			ft_unset(t_parser *lst, t_env **env);
 void			mini_remove_env(char *str, t_env **env);
 void			reasing_value(char *temp, char *str, t_env *head);
 
-//----Execution----//
-typedef struct s_execute
-{
-	int		fd_in;
-	int		fork_pid;
-	int		pipe_fd[2];
-	char	**path;
-	char	**env_array;
-}				t_execute;
 
 t_parser		*mini_forks(t_parser *lst, t_env *env, t_execute *data);
 bool			absolute_check(t_parser *node);
 bool			parse_path(t_env *env, t_execute *data);
 char			*check_access(t_env *env, t_parser *node, t_execute *data);
 void			ft_execute(t_env **env, t_parser *list);
+void			single_build(t_parser *lst, t_env **env, t_execute *data);
+// bool			single_build(t_parser *lst, t_env **env, t_execute *data);
+void			init_execute_struct(t_execute *data);
+bool			check_redirect(t_parser *node);
 void			build(t_parser *lst, t_env *env, t_execute *data);
-void			init_execute_struct(t_execute *data, t_env *env);
+void			free_data(t_execute *data);
 
 //----Utils----//
 void			mini_error(char *string, int error);
