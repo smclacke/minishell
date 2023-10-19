3e2f4eb207fa63ef713f60c626a438499dc337f2
YourGitHubUsername <YourGitHubEmail>
2023-10-19 23:04:33 +0200

    comment in djoyke.h

--- include/djoyke.h
+++ include/djoyke.h
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/28 14:04:53 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/10/19 23:00:13 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/10/19 23:04:13 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,8 +113,8 @@ typedef struct s_expand
 	char				*before_dollar;
 	char				*env_value;
 	char				*comp_str;
-	int					i;
-	int					j;
+	int					i;//are we using this?
+	int					j;//are we using this?
 }							t_expand;
 
 //----Execution----//
