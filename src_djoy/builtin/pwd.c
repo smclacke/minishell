093b88025157f0b58f63fd0c950e2e3ae7ecc892
YourGitHubUsername <YourGitHubEmail>
2023-10-31 19:29:28 +0100

    to-do list added

--- src_djoy/builtin/pwd.c
+++ src_djoy/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/19 21:23:37 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/10/19 22:05:45 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/10/31 19:15:59 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
  * @param path string passed as NULL that needs to be filled in this function
  * @brief get's current working firectory
  * @return node made
- * @todo also print the old one OLDPWD=/Users/dreijans/Documents/Rang 3/Minishell
- * needs exit status
+ * @todo needs exit status
 */
 void	ft_pwd(void)
 {
