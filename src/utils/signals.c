70d604751b6e4fa0f2d8ae5a4f3c5cced545ef72
smclacke <sarahmclacken@gmail.com>
2023-11-07 16:05:29 +0100

    forgot to switch branches

--- src/utils/signals.c
+++ src/utils/signals.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/05 17:40:06 by smclacke      #+#    #+#                 */
-/*   Updated: 2023/11/05 20:58:05 by smclacke      ########   odam.nl         */
+/*   Updated: 2023/11/07 16:05:13 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
