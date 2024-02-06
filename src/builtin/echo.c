6fdc26ca7fa5e076d90f68fb8ce4fc7079c7ba6c
YourGitHubUsername <YourGitHubEmail>
2024-02-06 16:08:40 +0100

    fixed echo

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/19 21:15:58 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/02/06 15:43:47 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/02/06 16:08:24 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@
  * @return returns true if sequence is -n(or multiple n)
  * returns false if [0] is not '-' and sequence is not 'n'
 */
-static bool	is_all_n(t_parser *temp)
+static bool	is_all_n(char *str)
 {
 	int	j;
 
 	j = 1;
-	if (temp->proc->str[0][0] != '-')
+	if (str[0] != '-')
 		return (false);
-	while (temp->proc->str[0][j] != '\0')
+	while (str[j] != '\0')
 	{
-		if (temp->proc->str[0][j] != 'n')
+		if (str[j] != 'n')
 			return (false);
 		j++;
 	}
@@ -37,18 +37,14 @@ static bool	is_all_n(t_parser *temp)
 /**
  * @param temp t_parser linked list
  * @brief writes string and space to the terminal
- * @todo pointer or indext at strlen?
 */
-static void	write_line(t_parser *temp)
+static void	write_line(t_procs *temp, int i)
 {
-	int i;
-
-	i = 0;
-	while (i < temp->proc->str_count)
+	while (i < temp->str_count)
 	{
-		if (temp->proc->str[i])
-			write(1, temp->proc->str[i], ft_strlen(temp->proc->str[i]));
-		if (i < temp->proc->str_count)
+		if (temp->str[i])
+			write(1, temp->str[i], ft_strlen(temp->str[i]));
+		if (i < (temp->str_count - 1))
 			write(1, " ", 1);
 		i++;
 	}
@@ -59,33 +55,24 @@ static void	write_line(t_parser *temp)
  * @param env environment linked list
  * @brief checks if str is "~" replaces it by HOME str
 */
-static void	home_check(t_parser *lst, t_env **env)
+static void	home_check(t_procs *lst, t_env **env)
 {
 	char		*new_str;
 
-	if (mini_strcmp(lst->proc->str[0], "~") == 0)
+	if (mini_strcmp(lst->str[0], "~") == 0)
 	{
 		new_str = ft_getenv(*env, "HOME");
-		lst->proc->str[0] = new_str;
+		lst->str[0] = new_str;
 	}
 }
 
 /**
  * @param lst t_parser linked list
  * @brief checks if cmd exist, and if there is a str input after it
- * @todo check error
-	// if (!temp->cmd)
-	// {
-	// 	mini_error(E_USAGE, lst);
-	// 	return (false);
-	// }
 */
-static bool	input_check(t_parser *lst)
+static bool	input_check(t_procs *lst)
 {
-	t_parser	*temp;
-
-	temp = lst;
-	if (lst->proc->str_count == 0)
+	if (lst->str_count == 0)
 	{
 		write(1, "\n", 1);
 		return (false);
@@ -98,26 +85,31 @@ static bool	input_check(t_parser *lst)
  * @brief writes node after command on standart output followed by /n char
  * -n that eliminates the endline char in output 
  * @return The echo utility exits 0 on success, and > 0 if an error occurs.
- * @todo error code, check if input function actually works
+ * @todo error code
 */
 void	ft_echo(t_parser *lst, t_env **env)
 {
 	t_parser	*temp;
 	int			is_flag;
+	int			i;
 
 	temp = lst;
 	is_flag = 0;
-	if (input_check(lst) == false)
+	i = 0;
+	if (input_check(temp->proc) == false)
 		return ;
-	home_check(temp, env);
-	if (is_all_n(temp))
+	home_check(temp->proc, env);
+	if (is_all_n(temp->proc->str[0]))
+	{
+		i++;
 		is_flag++;
-	if (is_flag != 0)
+	}
+	if (is_flag != 0 && temp->proc->str_count == 0)
 	{
 		lst->exit_code = E_USAGE;
 		return ;
 	}
-	write_line(temp);
+	write_line(temp->proc, i);
 	if (is_flag == 0)
 		write(1, "\n", 1);
 	lst->exit_code = E_USAGE;
