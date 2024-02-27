453d46b1209b2c0917f4ccb6c0b2e86404f4e378
YourGitHubUsername <YourGitHubEmail>
2024-02-27 17:06:26 +0100

    pwd fixed

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/19 21:23:37 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/02/24 19:02:15 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/02/27 17:02:40 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ void	ft_pwd(t_parser *head)
 	path = getcwd(NULL, 0);
 	if (path != NULL)
 	{
-		printf("Current working directory: %s\n", path);
+		printf("%s\n", path);
+		head->exit_code = E_USAGE;
 		free(path);
 	}
 	else
