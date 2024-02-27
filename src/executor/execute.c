cfc7af2f8c3478379c0b1c6655b0ee6568b891d6
YourGitHubUsername <YourGitHubEmail>
2024-02-27 16:59:56 +0100

    changes not needed

--- src/executor/execute.c
+++ src/executor/execute.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/02 13:56:26 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/02/26 23:52:42 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/02/27 16:50:00 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/25 17:34:44 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/02/27 16:41:39 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/02/27 16:59:39 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
