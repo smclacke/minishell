c36af66e8afdbc00b2067d991ce73e07ed0f89a9
smclacke <sarahmclacken@gmail.com>
2024-03-01 14:52:11 +0100

    norming meta_check.c

--- src/lexer/meta_check.c
+++ src/lexer/meta_check.c
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/26 20:05:11 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/02/27 14:06:13 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/03/01 14:51:23 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,21 @@ static int	meta_help(int c)
 	return (0);
 }
 
+static	int	check_pipe(char *input, int i)
+{
+	if (!input[i + 1] || (input[i + 1]
+			&& meta_help(input[i + 1] == 3)))
+		return (syntax_error("near unexpected token 'newline'"));
+	else
+	{
+		while (input[i] && ft_isspace(input[i]))
+			i++;
+		if (meta_help(input[i]) == 3 || !input[i])
+			return (syntax_error("near unexpected token 'newline'"));
+	}
+	return (i);
+}
+
 int	meta_check(char *input)
 {
 	int	i;
@@ -35,11 +50,27 @@ int	meta_check(char *input)
 			if (!input[i + 1] || (input[i + 1]
 					&& meta_help(input[i + 1]) == 3))
 				return (syntax_error("near unexpected token 'newline'"));
+			else
+			{
+				while (input[i] && ft_isspace(input[i]))
+					i++;
+				if (meta_help(input[i]) != 0 || !input[i])
+					return (syntax_error("near unexpected token 'newline'"));
+			}
 		}
 		if (meta_help(input[i]) == 3)
 		{
-			if (!input[i + 1] || (input[i + 1] && meta_help(input[i + 1] == 3)))
-				return (syntax_error("near unexpected token 'newline'"));
+			i = check_pipe(input, i);
+			// if (!input[i + 1] || (input[i + 1]
+			// 		&& meta_help(input[i + 1] == 3)))
+			// 	return (syntax_error("near unexpected token 'newline'"));
+			// else
+			// {
+			// 	while (input[i] && ft_isspace(input[i]))
+			// 		i++;
+			// 	if (meta_help(input[i]) == 3 || !input[i])
+			// 		return (syntax_error("near unexpected token 'newline'"));
+			// }
 		}
 		i++;
 	}
