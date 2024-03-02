a28e2d1f9b8e7663c11063a2390f62c8a4375c40
Sarah McLacken <SarahLouise@GUEST-01_SARAH-MACBOOK1.LAN>
2024-03-02 15:50:29 +0100

    changing branch

--- include/structs.h
+++ include/structs.h
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/17 16:42:25 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/03/01 18:59:26 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/03/02 15:49:57 by SarahLouise   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
